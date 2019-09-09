8c0f768a4ea224df312d49ad05957916bc282cbf
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-09-09 16:18:50 -0600

    Got the program running, next step is Preparing a Database

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Conrollers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
