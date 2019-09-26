b12a1d2f6a3487b78adbc5b56a9deb7949d0b464
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-09-26 01:37:56 -0600

    Finished Chapter 10. Added full cart and checkout functionality

---
+++ SportsStore/Migrations/20190926072715_fixOrders.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class fixOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartLine_Order_OrderID",
+                table: "CartLine");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "OrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartLine_Orders_OrderID",
+                table: "CartLine",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "OrderID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartLine_Orders_OrderID",
+                table: "CartLine");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "OrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartLine_Order_OrderID",
+                table: "CartLine",
+                column: "OrderID",
+                principalTable: "Order",
+                principalColumn: "OrderID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
