b9ed1ecc3020df11d17ec55786b981f641f018dc
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-09-28 13:48:01 -0600

    Finished Chapter 11, added Create and Delete methods

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -31,9 +31,22 @@ namespace SportsStore.Controllers
             }
             else
             {
-                // there is something wrong with the data values                
+                // there is something wrong with the data values
                 return View(product);
             }
+        }
+
+        public IActionResult Create() => View("Edit", new Product());
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+            return RedirectToAction("Index");
         }
     }
 }
